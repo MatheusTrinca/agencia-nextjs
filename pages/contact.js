d5330b0a7d80bac029ba414de456ad1d18ee0bb0
Matheus Trinca <matheusfelipetrinca@gmail.com>
2022-06-04 23:21:24 -0300

    contact

--- pages/contact.js
+++ pages/contact.js
@@ -5,15 +5,15 @@ import Circle from '../components/Circle';
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <Circle background="green" />
-      <Circle background="yellow" />
+      <Circle background="green" left="-40vh" top="-20vh" />
+      <Circle background="yellow" right="-30vh" bottom="-60vh" />
       <h1 className={styles.title}>ENTRE EM CONTATO</h1>
       <form className={styles.form}>
         <input type="text" className={styles.inputS} placeholder="Usuário" />
         <input type="text" className={styles.inputS} placeholder="Telefone" />
         <input type="email" className={styles.inputL} placeholder="Email" />
         <input type="text" className={styles.inputL} placeholder="Assunto" />
-        <textarea className={styles.textarea} placeholder="Mensagem" row={6} />
+        <textarea className={styles.textArea} placeholder="Mensagem" row={6} />
         <button className={styles.buttonF}>ENVIAR</button>
       </form>
     </div>
